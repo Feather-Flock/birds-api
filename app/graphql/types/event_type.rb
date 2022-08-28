0e88f9c2020731fcfa3016c035ad0170a96b533b
Clay Ash <claycourtneyash@gmail.com>
2022-08-28 14:01:34 -0600

    Add graphql files and createUser action

---
+++ app/graphql/types/event_type.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Types
+  class EventType < Types::BaseObject
+    field :id, ID, null: false
+    field :title, String
+    field :description, String
+    field :time, Types::TimeType
+    field :date, GraphQL::Types::ISO8601Date
+    field :lat, Float
+    field :lng, Float
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :address, String
+    field :city, String
+    field :state, String
+    field :zip, Integer
+  end
+end
