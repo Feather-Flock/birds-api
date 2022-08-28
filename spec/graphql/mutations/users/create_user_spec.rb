3f5b371871a343a62ada3fba30cee86b44d9ede0
Clay Ash <claycourtneyash@gmail.com>
2022-08-28 14:02:32 -0600

    Adjust user model test to not require image or description, add create_user spec

---
+++ spec/graphql/mutations/users/create_user_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe Mutations::CreateUser, type: :request do 
+  describe '.resolve' do 
+    it 'creates a user' do 
+      expect(User.count).to eq(0) 
+      post '/graphql', params: { query: query }
+      expect(User.count).to eq(1)
+    end
+
+    it 'returns a user' do 
+      post '/graphql', params: { query: query }
+      json = JSON.parse(response.body)
+      data = json['data']['createUser']
+      expect(data["user"]).to include(
+              "id"           => User.last.id.to_s,
+              "email"        => "garnet@universe.com",
+              "userName"     => 'Garnet',
+              "image"        => "https://image.png",
+              "description"  => "We are a married lesbian couple with kids. We love to play sports and go on adventures!",
+              "zipCode"      => 80220
+      )
+    end
+  end
+
+  def query
+    <<~GQL
+      mutation {
+        createUser(input: {
+          userName: "Garnet",
+          email: "garnet@universe.com",
+          image: "https://image.png",
+          description: "We are a married lesbian couple with kids. We love to play sports and go on adventures!",
+          zipCode: 80220
+       }) { user {
+          id
+          userName
+          email
+          description
+          image
+          zipCode
+        }
+        }
+      }
+    GQL
+  end 
+end 
+
