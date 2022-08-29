926e7bce46aeb8ee1ac508ca0a5f4c3e2b84f7be
Ian Ross <ianrossdrums@gmail.com>
2022-08-29 17:58:12 -0600

    test: map facade and vcr cassette

---
+++ spec/facades/map_facade_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe MapFacade do
+  describe 'create_cords' do
+    it 'returns lat and long cords', :vcr do
+      cords = MapFacade.create_cords("Denver,CO")
+
+      expect(cords).to include :lat, :lng
+
+      expect(cords[:lat]).to be_a(Float)
+      expect(cords[:lng]).to be_a(Float)
+    end
+  end
+end
