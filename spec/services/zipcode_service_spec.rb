6a17f5b4896e9c35a0cd5e93a431f844de2ce1b0
Ian Ross <ianrossdrums@gmail.com>
2022-08-30 14:45:09 -0600

    test: zipcode service and facade spec

---
+++ spec/services/zipcode_service_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe ZipcodeService do
+  describe 'get_zipcode_radius' do
+    it 'returns zipcodes for location', :vcr do
+      data = ZipcodeService.get_zipcode_radius("80516", 5)
+
+      expect(data[:zip_codes]).to be_an(Array)
+      expect(data[:zip_codes].count).to eq(3)
+      expect(data[:zip_codes][0]).to be_a(String)
+    end
+  end
+end
