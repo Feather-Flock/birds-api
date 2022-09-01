bc2e0336d389a64cd1519aedf923fb04924e8b6f
LucasPascale <lpluke5@gmail.com>
2022-09-01 14:35:00 -0500

    Update application.rb
    update formatting of cors config

--- config/application.rb
+++ config/application.rb
@@ -26,11 +26,7 @@ module BirdsApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource (
-          '*',
-          :headers :any, 
-          methods: [:post]
-        )
+        resource '*', :headers => :any, :methods => [:post]
       end
     end
   end
