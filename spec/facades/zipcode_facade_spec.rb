181e034b01ddbd85acbdc285d1b76e0bf60ad41f
Clay Ash <claycourtneyash@gmail.com>
2022-09-01 09:27:05 -0600

    Zipcode facade test for circle ci

--- spec/facades/zipcode_facade_spec.rb
+++ spec/facades/zipcode_facade_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ZipcodeFacade do
   describe 'create_zipcodes' do
-    it '', :vcr do
+    it 'finds area of zipcodes', :vcr do
       data = ZipcodeFacade.create_zipcodes("80516", 5)
 
       expect(data).to be_an(Array)
