bc0af6b9988ba6068ded1bbe914601be8c81a4eb
Deannah Burke <deannah@bierstadtlager.com>
2022-09-01 19:09:45 -0600

    Write destroy event resolve method, not yet working in test or dev

--- spec/graphql/mutations/events/destroy_event_spec.rb
+++ spec/graphql/mutations/events/destroy_event_spec.rb
@@ -2,34 +2,51 @@ require 'rails_helper'
 
 RSpec.describe Mutations::DestroyEvent, type: :request do
   describe '.resolve' do
+    it 'returns an event', :vcr do
+      user = User.create(user_name: "Garnet", email: "garnet@universe.com", image: "https://user-images.githubusercontent.com/99059063/187045147-667959c8-70f2-4fb3-b089-ca81f23a0310.png" , description: "We are a married lesbian couple with kids. We love to play sports and go on adventures!" , zip_code: 80220)
+      event = user.events.create(title: "Lunch at Denison Park", description: "We are getting together for a meet-and-greet at Denison Park.", time: '18:00:00', date: '2022-10-09', lat: "39.733", lng: "-104.904", address: "1105 Quebec St", city: "Denver", state: "CO", zip: 80220, host: user.id)
+
+      post '/graphql', params: { query: query(id: event.id, user_id: user.id) }
+
+      json = JSON.parse(response.body)
+      data = json['data']['updateEvent']
+
+      expect(data["event"]).to include(
+        "title"       => "Lunch at Denison Park",
+        "description" => "We are getting together for a meet-and-greet at Denison Park.",
+        "time"        => "18:00:00",
+        "date"        => "2022-10-09",
+        "address"     => "1105 Quebec St",
+        "city"        => "Denver",
+        "state"       => "CO",
+        "zip"         => 80220,
+        "host"        => user.id
+      )
+    end
+
     it 'destroys an event', :vcr do
       user = User.create(user_name: "Garnet", email: "garnet@universe.com", image: "https://user-images.githubusercontent.com/99059063/187045147-667959c8-70f2-4fb3-b089-ca81f23a0310.png" , description: "We are a married lesbian couple with kids. We love to play sports and go on adventures!" , zip_code: 80220)
       event = user.events.create(title: "Lunch at Denison Park", description: "We are getting together for a meet-and-greet at Denison Park.", time: '18:00:00', date: '2022-10-09', lat: "39.733", lng: "-104.904", address: "1105 Quebec St", city: "Denver", state: "CO", zip: 80220, host: user.id)
 
-      post '/graphql', params: { query: create_query(id: event.id, user_id: user.id) }
-
-      expect(Event.count).to eq(1)
-
-      post '/graphql', params: { query: destroy_query(id: event.id) }
-
-      expect(Event.count).to eq(0)
+      expect do
+        post '/graphql', params: { query: destroy_query(id: event.id) }
+      end.to change { Event.count }.by(-1)
     end
-  end
 
-  def create_query(id:, user_id:)
-    <<~GQL
-      mutation {
-        destroyEvent(input: {
-          id: #{id}
-          title: "Lunch at Denison Park",
-          description: "We are getting together for a meet-and-greet at Denison Park.",
-          time: "18:00:00",
-          date: "2022-10-09",
-          address: "1105 Quebec St",
-          city: "Denver",
-          state: "CO",
-          zip: 80220,
-          host: #{user_id}
+    def query(id:, user_id:)
+      <<~GQL
+        mutation {
+          updateEvent(input: {
+            id: #{id}
+            title: "Lunch at Denison Park",
+            description: "We are getting together for a meet-and-greet at Denison Park.",
+            time: "18:00:00",
+            date: "2022-10-09",
+            address: "1105 Quebec St",
+            city: "Denver",
+            state: "CO",
+            zip: 80220,
+            host: #{user_id}
            }) { event {
               title
               description
@@ -50,9 +67,18 @@ RSpec.describe Mutations::DestroyEvent, type: :request do
 
     def destroy_query(id:)
       <<~GQL
-          mutation {
-            destroyEvent(input: {
-              id: #{id}
+        mutation {
+          destroyEvent(input: {
+            id: #{id},
+            title: "Lunch at Denison Park",
+            description: "We are getting together for a meet-and-greet at Denison Park.",
+            time: "14:00:00",
+            date: "2022-10-09",
+            address: "1105 Quebec St",
+            city: "Denver",
+            state: "CO",
+            zip: 80220,
+           }) { event {
               title
               description
               time
@@ -63,11 +89,10 @@ RSpec.describe Mutations::DestroyEvent, type: :request do
               zip
               lat
               lng
-              host
-              }
             }
           }
         }
       GQL
     end
   end
+end
