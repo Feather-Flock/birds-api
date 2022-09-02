8e890918ddc51d4cb0117a7a4c5ed551ab993c6a
Deannah Burke <deannah@bierstadtlager.com>
2022-09-02 15:53:13 -0600

    Complete destroy event endpoint request spec

--- spec/graphql/mutations/events/destroy_event_spec.rb
+++ spec/graphql/mutations/events/destroy_event_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Mutations::DestroyEvent, type: :request do
   describe '.resolve' do
-    it 'returns an event', :vcr do
+    it 'destroys an event', :vcr do
       user = User.create(user_name: "Garnet", email: "garnet@universe.com", image: "https://user-images.githubusercontent.com/99059063/187045147-667959c8-70f2-4fb3-b089-ca81f23a0310.png" , description: "We are a married lesbian couple with kids. We love to play sports and go on adventures!" , zip_code: 80220)
       event = user.events.create(title: "Lunch at Denison Park", description: "We are getting together for a meet-and-greet at Denison Park.", time: '18:00:00', date: '2022-10-09', lat: "39.733", lng: "-104.904", address: "1105 Quebec St", city: "Denver", state: "CO", zip: 80220, host: user.id)
 
-      post '/graphql', params: { query: query(id: event.id, user_id: user.id) }
+      post '/graphql', params: { query: destroy_query(id: event.id) }
 
       json = JSON.parse(response.body)
-      data = json['data']['updateEvent']
+      data = json['data']['destroyEvent']
 
-      expect(data["event"]).to include(
+      expect(data).to include(
         "title"       => "Lunch at Denison Park",
         "description" => "We are getting together for a meet-and-greet at Denison Park.",
         "time"        => "18:00:00",
@@ -19,66 +19,17 @@ RSpec.describe Mutations::DestroyEvent, type: :request do
         "address"     => "1105 Quebec St",
         "city"        => "Denver",
         "state"       => "CO",
-        "zip"         => 80220,
-        "host"        => user.id
+        "zip"         => 80220
       )
-    end
-
-    it 'destroys an event', :vcr do
-      user = User.create(user_name: "Garnet", email: "garnet@universe.com", image: "https://user-images.githubusercontent.com/99059063/187045147-667959c8-70f2-4fb3-b089-ca81f23a0310.png" , description: "We are a married lesbian couple with kids. We love to play sports and go on adventures!" , zip_code: 80220)
-      event = user.events.create(title: "Lunch at Denison Park", description: "We are getting together for a meet-and-greet at Denison Park.", time: '18:00:00', date: '2022-10-09', lat: "39.733", lng: "-104.904", address: "1105 Quebec St", city: "Denver", state: "CO", zip: 80220, host: user.id)
-
-      expect do
-        post '/graphql', params: { query: destroy_query(id: event.id) }
-      end.to change { Event.count }.by(-1)
-    end
-
-    def query(id:, user_id:)
-      <<~GQL
-        mutation {
-          updateEvent(input: {
-            id: #{id}
-            title: "Lunch at Denison Park",
-            description: "We are getting together for a meet-and-greet at Denison Park.",
-            time: "18:00:00",
-            date: "2022-10-09",
-            address: "1105 Quebec St",
-            city: "Denver",
-            state: "CO",
-            zip: 80220,
-            host: #{user_id}
-           }) { event {
-              title
-              description
-              time
-              date
-              address
-              city
-              state
-              zip
-              lat
-              lng
-              host
-            }
-          }
-        }
-      GQL
+      expect(Event.count).to eq(0)
     end
 
     def destroy_query(id:)
       <<~GQL
         mutation {
           destroyEvent(input: {
-            id: #{id},
-            title: "Lunch at Denison Park",
-            description: "We are getting together for a meet-and-greet at Denison Park.",
-            time: "14:00:00",
-            date: "2022-10-09",
-            address: "1105 Quebec St",
-            city: "Denver",
-            state: "CO",
-            zip: 80220,
-           }) { event {
+            id: #{id}
+            }){
               title
               description
               time
@@ -91,7 +42,6 @@ RSpec.describe Mutations::DestroyEvent, type: :request do
               lng
             }
           }
-        }
       GQL
     end
   end
