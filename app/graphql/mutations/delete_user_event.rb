7349ca605af733def6f86629fad4c9cae9c04a13
Ian Ross <ianrossdrums@gmail.com>
2022-09-03 17:44:56 -0600

    remove pry

--- app/graphql/mutations/delete_user_event.rb
+++ app/graphql/mutations/delete_user_event.rb
@@ -1,12 +1,9 @@
 class Mutations::DeleteUserEvent < Mutations::BaseMutation
-  # argument :user_id, Integer, required: true
-  # argument :event_id, Integer, required: true
   argument :id, Integer, required: true
 
   type Types::UserEventType
 
   def resolve(id:)
-    #binding.pry
     UserEvent.find(id).delete
   end
 end
